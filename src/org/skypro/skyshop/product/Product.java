55820ca14e65231c16786522efb95766565b5770
testr <testrest136@gmail.com>
2025-04-20 19:11:27 +0300

    it's check

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,15 +1,16 @@
 package org.skypro.skyshop.product;
 
 public class Product {
-    private String name;
-    private int price;
-
+    String name;
+    int price;
+    public void infoProduct(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
     public String getName() {
         return name;
     }
-
     public int getPrice() {
         return price;
-
     }
 }
