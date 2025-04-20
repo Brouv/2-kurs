1a7f5de9a39f4af7ef320dae5db9b8241491be1b
testr <testrest136@gmail.com>
2025-04-20 19:11:27 +0300

    it's check

--- src/org/skypro/skyshop/Main.java
+++ src/org/skypro/skyshop/Main.java
@@ -2,6 +2,7 @@ package org.skypro.skyshop;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
+
 }
--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -1,5 +1,24 @@
 package org.skypro.skyshop.basket;
 
-public class ProductBasket {
 
+import org.skypro.skyshop.product.Product;
+
+public class ProductBasket {
+    public void addProduct(){
+    Product addProduct = new Product();
+    }
+    public void priceProduct(){
+
+    }
+    public void infoProduct(){
+
+    }
+    public void checkName(){
+
+    }
+    public void clearBasket(){
+
+    }
 }
+
+
