e74243a4c7285fc2f51d1a72912ec234f7fd2a2f
testr <testrest136@gmail.com>
2025-05-14 19:45:31 +0300

    it's check1

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,4 +1,15 @@
 package org.skypro.skyshop;
 
+import org.skypro.skyshop.product.Product;
+
 public class App {
+    public static void main(){
+        Product product = new Product("Апельсин" , 30);
+        Product product1 = new Product("Сыр", 170);
+        Product product2 = new Product("Гречка", 60);
+        Product product3 = new Product("Колбаса", 250);
+        Product product4 = new Product("Курица", 260);
+
+
+    }
 }
--- src/org/skypro/skyshop/Main.java
+++ src/org/skypro/skyshop/Main.java
@@ -2,5 +2,7 @@ package org.skypro.skyshop;
 
 public class Main {
     public static void main(String[] args) {
+
     }
+
 }
