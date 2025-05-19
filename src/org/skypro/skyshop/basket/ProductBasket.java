d1cab287e460c64b6b07b9a8bc654d9df3fc0636
testr <testrest136@gmail.com>
2025-05-19 20:02:39 +0300

    Merge remote-tracking branch '2kurs/Encapsulation-homework' into Encapsulation-homework

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -1,12 +1,9 @@
 package org.skypro.skyshop.basket;
 
-
 import org.skypro.skyshop.product.Product;
 
-
-
 public class ProductBasket {
-    private int index = 0;
+    int index = 0;
     private int size = 0;
     private Product[] products = new Product[5];
 
