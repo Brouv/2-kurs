0dc4ebfe2fa8d4b45249fb041ac27d1cdf48f9ee
testr <testrest136@gmail.com>
2025-05-19 20:43:27 +0300

    inheritance,abstract classes

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -3,7 +3,6 @@ package org.skypro.skyshop;
 import org.skypro.skyshop.basket.ProductBasket;
 import org.skypro.skyshop.product.Product;
 
-
 public class App {
     public static void main(String[] args){
 
