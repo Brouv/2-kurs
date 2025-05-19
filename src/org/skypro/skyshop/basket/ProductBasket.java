45480b5f3570faa78f01fc842a0e116de8e8a9d7
testr <testrest136@gmail.com>
2025-05-19 20:26:28 +0300

    it's check

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -2,7 +2,6 @@ package org.skypro.skyshop.basket;
 
 import org.skypro.skyshop.product.Product;
 
-
 public class ProductBasket {
     private int size = 0;
     private Product[] products = new Product[5];
