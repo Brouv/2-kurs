ddbcc45e40513366b7a30f466af5434993f44182
testr <testrest136@gmail.com>
2025-06-08 17:44:12 +0300

    inheritance,abstract classes

--- src/org/skypro/skyshop/Main.java
+++ src/org/skypro/skyshop/Main.java
@@ -1,13 +1,21 @@
 package org.skypro.skyshop;
 
 
+import org.skypro.skyshop.basket.ProductBasket;
 import org.skypro.skyshop.product.DiscountedProduct;
 import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.SimpleProduct;
 
 public class Main {
     public static void main(String[] args) {
-
+        SimpleProduct orange = new SimpleProduct("Апельсин", 100);
+        DiscountedProduct cucumber = new DiscountedProduct("Огурец",100,10);
+        FixPriceProduct cabbage = new FixPriceProduct("Капуста");
+        ProductBasket basket = new ProductBasket();
+        basket.add(orange);
+        basket.add(cucumber);
+        basket.add(cabbage);
+        System.out.println(basket);
     }
 }
 
